350450d5dc33b216c66609f3196856209efd3dd0
IvanNik17 <30436041+IvanNik17@users.noreply.github.com>
2021-06-04 13:59:28 +0200

    Data for VQVAE and CAE

---
+++ pre_processing/count_people.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  4 13:40:29 2021
+
+@author: IvanTower
+"""
+import numpy as np
+import pandas as pd
+
+
+
+
+
+
+if __name__ == '__main__':
+    
+    annotations_main_folder = r"people_manual_annotations"
